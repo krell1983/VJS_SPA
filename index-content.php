5326983a65e9581c98f6e7716929452c04820963
krell1983 <piotr.markiewicz.1983@gmail.com>
2016-08-18 23:10:03 +0200

    1.01v

--- index-content.php
+++ index-content.php
@@ -1,17 +1,17 @@
-<h1>SPA Vanilla JavaScript</h1>
+<h1>Single Page Application Vanilla JavaScript</h1>
 
 <p>
-  How hard is to make <b>SPA</b> using vanilla javascript <b>(VJS)</b>? Well it's not hard it's like 50 lines of code [<b>0.5kb</b>].
+  How hard, is to make <b>SPA - Single Page Application</b> using vanilla JavaScript? Well it's not hard it's like 50 lines of code [<b>0.5kb</b>].
 </p>
 
 <h1>Basic Concept</h1>
 
 <p>
-    Web sites that works like <b>SPA</b> and is working like normal web page when <b>JS</b> is not available. <br>
-    Java Script will take control over link in main menu and turn them into AJAX calls that will download only page content.
+     Web sites that work like SPA are working like normal web page when JS is not available.<br>
+    Java Script will take control over links in main menu and turn them into AJAX calls that will download page content only.
 </p>
 
 
 <p class="center"><br>
-    <a href="https://github.com/krell1983/VJS_SPA" target="_blank">  GIT HUB LINK </a>
+    <a href="https://github.com/krell1983/VJS_SPA" target="_blank"> GIT HUB LINK </a>
 </p>
